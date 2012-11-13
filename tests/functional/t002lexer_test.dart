6fd051d313edfacd45c6ef6d8786d83bd367d94d
Gubaer <karl.guggisberg@guggis.ch>
2012-11-13 21:42:32 +0100

    fixed first five test cases

--- tests/functional/t002lexer_test.dart
+++ tests/functional/t002lexer_test.dart
@@ -2,12 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#library("dart:t002lexer_test");
+library t002lexer_test;
 
-#import("../../lib/unittest/unittest.dart");
-#import("../../lib/unittest/vm_config.dart");
-#import("../out/t002lexer.dart");
-#import("../../src/DartLRLib.dart");
+import "package:unittest/unittest.dart";
+import "package:dartlr/vm_config.dart";
+import "package:dartlr/dartlr.dart";
+
+import "../out/t002lexer.dart";
 
 main() {  
   useVmConfiguration();
@@ -17,13 +18,13 @@ main() {
     Lexer lexer = new t002lexer(stream);
     
     Token token = lexer.nextToken();   
-    expect(token.type).equals(t002lexer.ZERO);
+    expect(token.type,equals(t002lexer.ZERO));
     
     token = lexer.nextToken();    
-    expect(token.type).equals(t002lexer.ONE);
+    expect(token.type,equals(t002lexer.ONE));
     
     token = lexer.nextToken();    
-    expect(token.type).equals(t002lexer.EOF);
+    expect(token.type,equals(t002lexer.EOF));
   });
   
   test('testMalformedInput_t002lexer', () {
@@ -31,6 +32,6 @@ main() {
     Lexer lexer = new t002lexer(stream);
     Token token = lexer.nextToken();
     String expected = "line 1:0 no viable alternative at character '2'";
-    expect(lexer.reportedErrors.last()).equals(expected);
+    expect(lexer.reportedErrors.last(),equals(expected));
   });
 }
--- tests/functional/t003lexer_test.dart
+++ tests/functional/t003lexer_test.dart
@@ -2,12 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#library("dart:t003lexer_test");
+library t003lexer_test;
 
-#import("../../lib/unittest/unittest.dart");
-#import("../../lib/unittest/vm_config.dart");
-#import("../out/t003lexer.dart");
-#import("../../src/DartLRLib.dart");
+import "package:unittest/unittest.dart";
+import "package:dartlr/vm_config.dart";
+import "package:dartlr/dartlr.dart";
+
+import "../out/t003lexer.dart";
 
 main() {  
   useVmConfiguration();
@@ -17,16 +18,16 @@ main() {
     Lexer lexer = new t003lexer(stream);
     
     Token token = lexer.nextToken();   
-    expect(token.type).equals(t003lexer.ZERO);
+    expect(token.type, equals(t003lexer.ZERO));
     
     token = lexer.nextToken();
-    expect(token.type).equals(t003lexer.FOOZE);
+    expect(token.type, equals(t003lexer.FOOZE));
     
     token = lexer.nextToken();
-    expect(token.type).equals(t003lexer.ONE);
+    expect(token.type, equals(t003lexer.ONE));
     
     token = lexer.nextToken();    
-    expect(token.type).equals(t003lexer.EOF);
+    expect(token.type, equals(t003lexer.EOF));
   });
   
   test('testMalformedInput_t003lexer', () {
@@ -34,6 +35,6 @@ main() {
     Lexer lexer = new t003lexer(stream);
     Token token = lexer.nextToken();
     String expected = "line 1:0 no viable alternative at character '2'";
-    expect(lexer.reportedErrors.last()).equals(expected);
+    expect(lexer.reportedErrors.last(),equals(expected));
   });
 }
